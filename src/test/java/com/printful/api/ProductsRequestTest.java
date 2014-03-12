838d908a87059874fb2fc00af91def4265aeeb83
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-03-12 10:15:45 -0400

    I'm trying to hit the url that bruno suggested on S.O.

--- src/test/java/com/printful/api/ProductsRequestTest.java
+++ src/test/java/com/printful/api/ProductsRequestTest.java
@@ -18,7 +18,7 @@ public class ProductsRequestTest
     public void testGetAllProducts()
     {
         ProductsRequest request = new ProductsRequest("aewco3iz-8ta7-4ipp:qijx-u83bmxs9jie8");
-        List<Product> products = request.getAllProductList("https://api.theprintful.com/products");
+        List<Product> products = request.getAllProductList("https://sni.velox.ch/");
         assertNotNull(products);
     }
 }
