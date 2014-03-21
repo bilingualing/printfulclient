e6f12ca2042663b34301e3709a41ea7fd7483ad5
jamaal.taylor <j.jerrod.taylor@gmail.com>
2014-03-21 15:24:05 -0400

    I'm writing a request object for orders

---
+++ src/main/java/com/printful/api/RequestObjects/OrdersRequest.java
@@ -0,0 +1,47 @@
+package com.printful.api.RequestObjects;
+
+import com.printful.api.Orders.Order;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 3/21/14
+ * Time: 2:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrdersRequest
+{
+
+    public List<Order> getListOfOrders()
+    {
+
+    }
+
+    public Order createNewOrder()
+    {
+
+    }
+
+    public Order getOrderData()
+    {
+
+    }
+
+    public Order cancelOrder()
+    {
+
+    }
+
+    public Order updateOrder()
+    {
+
+    }
+
+    public Order confirmDraftForFulfillment()
+    {
+
+    }
+
+}
